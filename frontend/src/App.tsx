7c33497a15ccc806cbaec613a7028ef6bad5e209
Kuroma <arconiel456@gmai.com>
2024-06-18 16:52:03 +0300

    feat: add react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Api} from "./components/Api.component";
+
+function App() {
+  return (
+    <div className="App">
+      <Api />
+    </div>
+  );
+}
+
+export default App;
